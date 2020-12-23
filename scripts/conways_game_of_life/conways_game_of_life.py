e375ef9da65200e414a34eb220976743c35f8680
yardenas <yardas@student.ethz.ch>
2020-12-23 18:54:21 +0100

    training logging

--- scripts/conways_game_of_life/conways_game_of_life.py
+++ scripts/conways_game_of_life/conways_game_of_life.py
@@ -1,3 +1,4 @@
+import collections
 import os
 
 import matplotlib
@@ -43,7 +44,7 @@ def show_sequence(sequence, figname=None):
     out[:, 1::width, :] = 0.5
     plt.imshow(out, cmap=matplotlib.cm.Greys_r)
     if figname is not None:
-      plt.savefig(figname, 'png')
+        plt.savefig(figname)
 
 
 def make_dataset(dir, prefix='train', repeat=0, shuffle=0, seed=0):
@@ -123,25 +124,29 @@ def main():
     model = models.WorldModel('binary_image', (64, 64, 1), 30, 300, 256, 0)
     optimizer = tf.keras.optimizers.Adam(
         learning_rate=5e-4, clipnorm=100, epsilon=1e-5)
-    train_dataset = make_dataset('train_dataset', repeat=5, shuffle=0)
-    train_loss = tf.keras.metrics.Mean()
+    train_dataset = make_dataset('train_dataset', repeat=1, shuffle=0)
+    config = collections.namedtuple('Config', ['log_dir'])('results')
+    logger = utils.TrainingLogger(config)
     for i, batch in enumerate(train_dataset):
         grads, loss, log_p_obs, total_kl = grad(model, batch)
-        train_loss.update_state(loss)
+        logger['loss'].update_state(loss)
+        logger['log_probs'].update_state(log_p_obs)
+        logger['kl'].update_state(total_kl)
         optimizer.apply_gradients(zip(grads, model.trainable_variables))
         if (i % 50) == 0:
             print("Loss: {}\nObs log probs: {}\nKL: {}".format(
-                train_loss.result(), log_p_obs, total_kl))
+                logger['loss'].result(), logger['log_probs'].result(), logger['kl'].result()))
+            logger.log_metrics(i)
 
     test_dataset = make_dataset('test_dataset', 'test')
-    test_elbo = tf.keras.metrics.Mean()
     for i, batch in enumerate(test_dataset):
         reconstructed_sequence, elbo = reconstruct(model, batch)
-        test_elbo.update_state(elbo)
+        logger['test_elbo'].update_state(elbo)
         if (i % 50) == 0:
-            print("Test ELBO: {}".format(test_elbo.result()))
-            show_sequence(batch[:3, ::5, ...], 'test_image' + str(i))
-	    show_sequence(reconstructed_sequence[:3, ::5, ...], 'reconstructed_image' + str(i))
+            print("Test ELBO: {}".format(logger['test_elbo'].result()))
+            show_sequence(batch[:3, ::5, ...], 'results/test_image_' + str(i) + '.png')
+            show_sequence(reconstructed_sequence[:3, ::5, ...],
+                          'results/reconstructed_image_' + str(i) + '.png')
 
 
 if __name__ == '__main__':
