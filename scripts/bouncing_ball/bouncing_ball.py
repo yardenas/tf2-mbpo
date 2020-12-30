69029236e4dbcb7d9f94e473bf0ec20810c0d994
yardenas <yardas@student.ethz.ch>
2020-12-30 14:34:10 +0100

    ensemble with sgd

--- scripts/bouncing_ball/bouncing_ball.py
+++ scripts/bouncing_ball/bouncing_ball.py
@@ -77,12 +77,12 @@ def main():
     np.random.seed(0)
     config_dict = train_utils.define_config()
     config_dict['observation_type'] = 'binary_image'
-    config_dict['model_learning_rate'] = 1e-4
+    config_dict['model_learning_rate'] = 5e-5
     config_dict['posterior_samples'] = 1
-    config_dict['log_dir'] = 'results_swag_sgd_1e-4'
+    config_dict['log_dir'] = 'results_ensemble_5e-5'
     config = train_utils.make_config(config_dict)
     logger = utils.TrainingLogger(config)
-    model = SwagWorldModel(config, logger, (64, 64, 1))
+    model = EnsembleWorldModel(config, logger, (64, 64, 1))
     train_dataset = make_dataset('dataset', repeat=1, shuffle=0,
                                  batch_size=16)
     global_step = 0
