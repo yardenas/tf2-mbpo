160ae035fa5b7e4976f95883e4127cd19e5a9249
yardenas <yardas@student.ethz.ch>
2021-01-28 11:16:02 +0100

    inverted noise

--- experiments/inverted_pendulum/collect_dataset.py
+++ experiments/inverted_pendulum/collect_dataset.py
@@ -5,6 +5,17 @@ import gym
 import numpy as np
 
 
+class RandomObs(gym.ObservationWrapper):
+    def __init__(self, env, stddev=0.1):
+        super(RandomObs,
+              self).__init__(env)
+        self._stddev = stddev
+
+    def observation(self, observation):
+        return observation + np.random.normal(np.zeros_like(observation),
+                                              np.ones_like(observation)) * self._stddev
+
+
 def generate_sequence(env, horizon):
     obs_sequence = np.empty((horizon,) + env.observation_space.shape)
     act_sequence = np.empty((horizon - 1,) + env.action_space.shape)
@@ -23,13 +34,13 @@ def generate_sequence(env, horizon):
     return obs_sequence, act_sequence
 
 
-if __name__ == "__main__":
+def main():
     T = 50
     N = 100
     M = 200
     target_dir = os.path.join('data', 'inverted_pendulum')
     os.makedirs(target_dir, exist_ok=True)
-    env = gym.make('Pendulum-v0')
+    env = RandomObs(gym.make('Pendulum-v0'))
     for j in range(M):
         print('.', end='')
         sys.stdout.flush()
@@ -48,3 +59,7 @@ if __name__ == "__main__":
             obs_data[i], action_data[i] = generate_sequence(env, T)
         np.savez_compressed(os.path.join(target_dir, 'test_%03d' % j),
                             observation=obs_data, action=action_data)
+
+
+if __name__ == "__main__":
+    main()
